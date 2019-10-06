81804e45779984321f41097bedba9b7f07f7520a
Velis <velis@Veliss-Mac.local>
2019-10-06 20:11:51 +0300

    Initial Commit

---
+++ Proba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proba
+//
+//  Created by Velis on 6.10.19.
+//  Copyright © 2019 Go Easy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
