4986ad1ce432813ead12db7b22f8c5193745d969
Velis <velis@Veliss-Mac.local>
2019-10-06 21:21:36 +0300

    Adding webView

--- Proba/ViewController.swift
+++ Proba/ViewController.swift
@@ -5,14 +5,26 @@
 //  Created by Velis on 6.10.19.
 //  Copyright © 2019 Go Easy. All rights reserved.
 //
-
+import WebKit
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView:WKWebView!
+    
+    override func loadView(){
+        webView=WKWebView()
+        webView.navigationDelegate=self
+        view=webView
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string:"https://demo1.easy.bg")!
+        webView.load(URLRequest(url:url))
+        webView.allowsBackForwardNavigationGestures=true
     }
 
 
